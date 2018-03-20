69bdc7475d19627f14f68174f22a93c3a5cc88c3
Zhenyun Yu <yzyDavid@qq.com>
2018-03-20 18:14:36 +0800

    init form.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,11 +1,15 @@
 import {Component} from 'react';
 import * as React from 'react';
+import {WrappedLoginForm} from './LoginForm';
+import DvaProps from '../models/DvaProps';
 
-export default class HomePageComponent extends Component {
+export default class HomePageComponent extends Component implements DvaProps {
+    public dispatch: any;
+
     render() {
         return (
             <div>
-                <h2>登录</h2>
+                <WrappedLoginForm dispatch={this.dispatch}/>
             </div>
         );
     }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,10 +1,11 @@
-import {Component, ReactNode} from 'react';
+import {Component, FormEvent, ReactNode} from 'react';
 import * as React from 'react';
-import {Form, Button} from 'antd';
+import {Icon, Form, Button, Input} from 'antd';
+import DvaProps from '../models/DvaProps';
 
 const FormItem = Form.Item;
 
-interface FormProps {
+interface FormProps extends DvaProps {
     form: any;
 }
 
@@ -12,7 +13,9 @@ export class LoginForm extends Component<FormProps, any> {
     componentDidMount() {
     }
 
-    handleSubmit(e: any) {
+    handleSubmit(e: FormEvent<any>) {
+        e.preventDefault();
+        const formProps = this.props.form;
     }
 
     render() {
@@ -29,8 +32,28 @@ export class LoginForm extends Component<FormProps, any> {
         };
         return (
             <Form onSubmit={this.handleSubmit}>
-                <FormItem>
-
+                <FormItem label="统一身份编号" {...formItemLayout} hasFeedback>
+                    {
+                        getFieldDecorator('uid', {
+                            rules: [
+                                {required: true, message: '请输入学号或教工号'},
+                                {pattern: /^[0-9]+$/, message: '请输入数字'}
+                            ]
+                        })(
+                            <Input prefix={<Icon type="user" style={{fontSize: 13}}/>}/>
+                        )
+                    }
+                </FormItem>
+                <FormItem label="密码" {...formItemLayout} hasFeedback>
+                    {
+                        getFieldDecorator('password', {
+                            rules: [
+                                {required: true, message: '请输入密码'}
+                            ]
+                        })(
+                            <Input prefix={<Icon type="password" style={{fontSize: 13}}/>}/>
+                        )
+                    }
                 </FormItem>
             </Form>
         );
