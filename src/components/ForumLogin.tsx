32ffd6c9908716dcc8d5cf79d8fca4dbae671e96
OneMore14 <867846680@qq.com>
2018-04-22 20:51:10 +0800

    add css

--- src/components/ForumLogin.tsx
+++ src/components/ForumLogin.tsx
@@ -1,7 +1,8 @@
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 import * as React from 'react'
-import HomePageComponent from "./HomePage";
-const FormItem = Form.Item;
+import './css/ForumLogin.css'
+import FormItem from "antd/es/form/FormItem";
+
 
 export default class ForumLoginComponent extends React.Component {
 
@@ -10,20 +11,20 @@ export default class ForumLoginComponent extends React.Component {
         return (
             <Form  className="login-form">
                 <FormItem>
-                    <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+                    <Input  prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="学号" />
 
                 </FormItem>
                 <FormItem>
-                    <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+                    <Input  prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="密码" />
                 </FormItem>
                 <FormItem>
-                    <Checkbox>Remember me</Checkbox>
-                    <a className="login-form-forgot">Forgot password</a>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
+                    <Checkbox>记住我</Checkbox>
+                    <a style={{float:150}} className="login-form-forgot">忘记密码</a>
+                    <Button  type="primary" htmlType="submit" className="login-form-button">
+                        登录
                     </Button>
-
                 </FormItem>
+
             </Form>
         );
     }
