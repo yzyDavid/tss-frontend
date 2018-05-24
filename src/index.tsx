6b76d4cf1b7ee19aee453084b5e31103b5770cb5
OneMore14 <867846680@qq.com>
2018-05-24 10:01:09 +0800

    修改变量名

--- src/index.tsx
+++ src/index.tsx
@@ -2,42 +2,35 @@ import dva, {connect} from 'dva';
 import {Router, Route, Switch, routerRedux, browserHistory} from 'dva/router';
 import * as React from 'react';
 import {Layout, message} from 'antd';
-import {TssFooter, TssHeader} from './components/ForumNavigation';
-import ForumHomePageComponent from './components/ForumHomePage';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import NavigationPageComponent from './components/NavigationPage';
-import UserPageComponent from './components/UserPage';
-import UserManagePageComponent from './components/UserManagePage';
-import NavigationModel from './models/forumNavigationModel';
-import UserInfoModel from './models/userInfoModel';
+import ForumHomePageComponent from './components/ForumHomePage';
 import ForumUserPageComponent from './components/ForumUserPage'
 import MyPostPageComponent from './components/ForumMyPostPage'
 import ReplyPageComponent from './components/ForumReplyPage'
 import NewTopicPageComponent from './components/ForumNewTopicPage'
 import LetterPageComponent from './components/ForumMailPage'
-import SearchModel from './models/forumSearchModel'
-import AllBoardModel from './models/forumAllBoardModel'
-import MyPostModel from './models/forumMyPostModel'
-import MyBoardModel from './models/forumMyboardModel'
-import TopicModel from './models/forumTopicModel'
 import BoardPageComponent from './components/ForumBoardPage'
 import TopicPageComponent from './components/ForumTopicPage'
-import ReplyListModel from './models/forumReplyListModel'
-import ForumHomeModel from './models/forumHomeModel'
-import BoardMOdel from './models/forumBoardModel'
 import SearchComponent from "./components/ForumSearchPage"
 import AllBoardComponent from "./components/ForumAllBoard"
-import MailModel from "./models/forumMailModel"
-import ForumUserModel from "./models/forumUserModel"
-import {Simulate} from "react-dom/test-utils";
 
-import input = Simulate.input;
+import ForumTopicModel from './models/forumTopicModel'
+import ForumReplyListModel from './models/forumReplyListModel'
+import ForumHomeModel from './models/forumHomeModel'
+import ForumBoardMOdel from './models/forumBoardModel'
+import ForumSearchModel from './models/forumSearchModel'
+import ForumAllBoardModel from './models/forumAllBoardModel'
+import ForumMyPostModel from './models/forumMyPostModel'
+import ForumMyBoardModel from './models/forumMyboardModel'
+import ForumUserModel from "./models/forumUserModel"
+import ForumMailModel from "./models/forumMailModel"
+import ForumNavigationModel from './models/forumNavigationModel';
+
 
 const {Content} = Layout;
 
 
-const HomePage = connect(state => {
+const ForumHomePage = connect(state => {
     const dataSource = state.allboard.uid;
     const mylist = state.myboard.list;
     const hot = state.forumhome.HotList;
@@ -45,48 +38,48 @@ const HomePage = connect(state => {
     return {uid: dataSource,alllist :state.allboard.list,mylist:mylist,hot:hot,latest:latest };
 })(ForumHomePageComponent);
 
-const UserPage = connect(state => {
+const ForumUserPage = connect(state => {
     const data = state.ForumUserInfo.userInfo;
     return {userInfo:data};
 })(ForumUserPageComponent);
 
-const MyPostPage = connect(state => {
+const ForumMyPostPage = connect(state => {
     const data = state.mypost.postList;
     return{postList :data};
 })(MyPostPageComponent);
 
-const LetterPage = connect(state => {
+const ForumLetterPage = connect(state => {
     const data = state.mail.input;
     return {allstate:data}
 })(LetterPageComponent);
 
-const NewTopicPage = connect(state => {
+const ForumNewTopicPage = connect(state => {
     const data= {boardName: state.board.boardState.BoardName,boardID:state.board.boardState.BoardID};
     return {topicBoardInfo:data}
 })(NewTopicPageComponent);
 
-const BoardPage = connect(state => {
+const ForumBoardPage = connect(state => {
     const data = state.board.boardState;
 
     return { boardinfo:data}
 })(BoardPageComponent);
 
-const TopicPage = connect(state => {
+const ForumTopicPage = connect(state => {
     const data = state.topic.allstate;
     return {allstate: data};
 })(TopicPageComponent);
 
-const ReplyPage = connect(state => {
+const ForumReplyPage = connect(state => {
     const data = state.replyList.replylist;
     return {ReplyList: data};
 })(ReplyPageComponent);
 
-const SearchPage = connect(state => {
+const ForumSearchPage = connect(state => {
     const data = state.search.data;
     return {boardList:data}
 })(SearchComponent);
 
-const AllBoardPage = connect(state => {
+const ForumAllBoardPage = connect(state => {
     const data = state.allboard.list;
     return {boardList:data};
 })(AllBoardComponent);
@@ -99,17 +92,16 @@ const app = dva({
 
 
 
-app.model(NavigationModel);
-app.model(MyBoardModel);
-app.model(AllBoardModel);
-app.model(MyPostModel);
-app.model(TopicModel);
+app.model(ForumNavigationModel);
+app.model(ForumMyBoardModel);
+app.model(ForumAllBoardModel);
+app.model(ForumMyPostModel);
+app.model(ForumTopicModel);
 app.model(ForumHomeModel);
-app.model(BoardMOdel);
-app.model(ReplyListModel);
-app.model(MailModel);
-app.model(UserInfoModel);
-app.model(SearchModel);
+app.model(ForumBoardMOdel);
+app.model(ForumReplyListModel);
+app.model(ForumMailModel);
+app.model(ForumSearchModel);
 app.model(ForumUserModel);
 
 
@@ -120,18 +112,18 @@ app.router(({history}) => (
                 <Content style={{minHeight: '300px'}}>
                     <Switch>
 
-                        <Route exact path="/home"  component={HomePage}/>
-                        <Route exact path="/userinfo" component={UserPage}/>
-                        <Route  path="/uid=:uid" component={UserPage}/>
-                        <Route exact path="/userarticle" component={UserPage}/>
-                        <Route exact path="/mypost" component={MyPostPage}/>
-                        <Route exact path="/privateLetter" component={LetterPage}/>
-                        <Route exact path="/reply" component={ReplyPage}/>
-                        <Route exact path="/search" component={SearchPage}/>
-                        <Route exact path="/newpost" component={NewTopicPage}/>
-                        <Route exact path="/allboard" component={AllBoardPage}/>
-                        <Route path="/board=:boardid" component={BoardPage}/>
-                        <Route path="/topic=:topicid" component={TopicPage}/>
+                        <Route exact path="/home"  component={ForumHomePage}/>
+                        <Route exact path="/userinfo" component={ForumUserPage}/>
+                        <Route  path="/uid=:uid" component={ForumUserPage}/>
+                        <Route exact path="/userarticle" component={ForumUserPage}/>
+                        <Route exact path="/mypost" component={ForumMyPostPage}/>
+                        <Route exact path="/privateLetter" component={ForumLetterPage}/>
+                        <Route exact path="/reply" component={ForumReplyPage}/>
+                        <Route exact path="/search" component={ForumSearchPage}/>
+                        <Route exact path="/newpost" component={ForumNewTopicPage}/>
+                        <Route exact path="/allboard" component={ForumAllBoardPage}/>
+                        <Route path="/board=:boardid" component={ForumBoardPage}/>
+                        <Route path="/topic=:topicid" component={ForumTopicPage}/>
                     </Switch>
                 </Content>
             </Layout>
