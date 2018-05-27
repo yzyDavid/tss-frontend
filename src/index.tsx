0cfa7fe2f850b940850f60aea8ce371f08b9c756
OneMore14 <867846680@qq.com>
2018-05-27 13:22:50 +0800

    添加A4文件

--- src/index.tsx
+++ src/index.tsx
@@ -40,6 +40,29 @@ import ClassSelectionComponent from './components/SelectionClass';
 import StudentListComponent from './components/StudentList';
 
 
+import ForumHomePageComponent from './components/ForumHomePage';
+import ForumNavigationModel from './models/forumNavigationModel';
+import ForumUserInfoModel from './models/forumUserInfoModel';
+import ForumUserPageComponent from './components/ForumUserPage'
+import MyPostPageComponent from './components/ForumMyPostPage'
+import ReplyPageComponent from './components/ForumReplyPage'
+import NewTopicPageComponent from './components/ForumNewTopicPage'
+import LetterPageComponent from './components/ForumMailPage'
+import ForumSearchModel from './models/forumSearchModel'
+import ForumAllBoardModel from './models/forumAllBoardModel'
+import ForumMyPostModel from './models/forumMyPostModel'
+import ForumMyBoardModel from './models/forumMyboardModel'
+import ForumTopicModel from './models/forumTopicModel'
+import BoardPageComponent from './components/ForumBoardPage'
+import TopicPageComponent from './components/ForumTopicPage'
+import ForumReplyListModel from './models/forumReplyListModel'
+import ForumHomeModel from './models/forumHomeModel'
+import ForumBoardMOdel from './models/forumBoardModel'
+import SearchComponent from "./components/ForumSearchPage"
+import AllBoardComponent from "./components/ForumAllBoard"
+import ForumMailModel from "./models/forumMailModel"
+import ForumUserModel from "./models/forumUserModel"
+
 const {Content} = Layout;
 
 const app = dva({
@@ -54,6 +77,20 @@ app.model(CourseInfoModel);
 app.model(CurriculumTeacherModel);
 app.model(CurriculumManageModel);
 app.model(ClassroomManageModel);
+
+
+app.model(ForumNavigationModel);
+app.model(ForumMyBoardModel);
+app.model(ForumAllBoardModel);
+app.model(ForumMyPostModel);
+app.model(ForumTopicModel);
+app.model(ForumHomeModel);
+app.model(ForumBoardMOdel);
+app.model(ForumReplyListModel);
+app.model(ForumMailModel);
+app.model(ForumUserInfoModel);
+app.model(ForumSearchModel);
+app.model(ForumUserModel);
 
 const HomePage = connect(state => {
     return {}
@@ -142,6 +179,67 @@ const StuListPage = connect(state =>{
     const {dataSource} = state.studentList;
     return {dataSource: dataSource};
 })(StudentListComponent)
+
+
+
+const ForumTopicPage = connect(state => {
+    const data = state.topic.allstate;
+    return {allstate: data};
+})(TopicPageComponent);
+
+const ForumReplyPage = connect(state => {
+    const data = state.replyList.replylist;
+    return {ReplyList: data};
+})(ReplyPageComponent);
+
+const ForumSearchPage = connect(state => {
+    const data = state.search.data;
+    return {boardList:data}
+})(SearchComponent);
+
+const ForumAllBoardPage = connect(state => {
+    const data = state.allboard.list;
+    return {boardList:data};
+})(AllBoardComponent);
+
+
+const ForumHomePage = connect(state => {
+    const dataSource = state.allboard.uid;
+    const mylist = state.myboard.list;
+    const hot = state.forumhome.HotList;
+    const latest = state.forumhome.LatestList;
+    return {uid: dataSource,alllist :state.allboard.list,mylist:mylist,hot:hot,latest:latest };
+})(ForumHomePageComponent);
+
+const ForumUserPage = connect(state => {
+    const data = state.ForumUserInfo.userInfo;
+    return {userInfo:data};
+})(ForumUserPageComponent);
+
+const ForumMyPostPage = connect(state => {
+    const data = state.mypost.postList;
+    return{postList :data};
+})(MyPostPageComponent);
+
+
+const ForumLetterPage = connect(state => {
+    const data = state.mail.input;
+    return {allstate:data}
+})(LetterPageComponent);
+
+const ForumNewTopicPage = connect(state => {
+    const data= {boardName: state.board.boardState.BoardName,boardID:state.board.boardState.BoardID};
+    return {topicBoardInfo:data}
+})(NewTopicPageComponent);
+
+const ForumBoardPage = connect(state => {
+    const data = state.board.boardState;
+
+    return { boardinfo:data}
+})(BoardPageComponent);
+
+
+
 app.router(({history}) => (
         <Router history={history}>
             <Layout>
@@ -166,6 +264,20 @@ app.router(({history}) => (
                         <Route path="/stuSelect" component={StuSelectPage}/>
                         <Route path="/classSelect/:courseId" component={ClassSelectPage}/>
                         <Route path="/stuList/:classId" component={StuListPage}/>
+
+                        <Route exact path="/home"  component={ForumHomePage}/>
+                        <Route exact path="/userinfo" component={ForumUserPage}/>
+                        <Route  path="/uid=:uid" component={ForumUserPage}/>
+                        <Route exact path="/userarticle" component={ForumUserPage}/>
+                        <Route exact path="/mypost" component={ForumMyPostPage}/>
+                        <Route exact path="/privateLetter" component={ForumLetterPage}/>
+                        <Route exact path="/reply" component={ForumReplyPage}/>
+                        <Route exact path="/search" component={ForumSearchPage}/>
+                        <Route exact path="/newpost" component={ForumNewTopicPage}/>
+                        <Route exact path="/allboard" component={ForumAllBoardPage}/>
+                        <Route path="/board=:boardid" component={ForumBoardPage}/>
+                        <Route path="/topic=:topicid" component={ForumTopicPage}/>
+
                     </Switch>
                 </Content>
             <TssFooter/>
---
+++ src/models/forumHomeModel.ts
@@ -0,0 +1,114 @@
+import {routerRedux} from 'dva/router';
+import {tssFetch} from '../utils/tssFetch';
+
+const model = {
+    namespace: 'forumhome',
+    state: {
+
+        HotList : [{
+            key: '1',
+            board: '数据库-高云君',
+            title: "这是一个帖子的标题",
+            author: "我是发帖人ID",
+            boardid:'123',
+            topicid:'123',
+            time:"2018-05-01 12:00:00",
+            replyNUM:"5"
+        }, {
+            key: '2',
+            board: '数据结构基础-陈越',
+            title: "这是一个帖子的标题",
+            author: "我是发帖人ID",
+            boardid:'123',
+            topicid:'123',
+            time:"2018-05-01 12:00:00",
+            replyNUM:"5"
+        }, {
+            key: '3',
+            board: '计算机组成-楼学庆',
+            title: "这是一个帖子的标题",
+            author: "我是发帖人ID",
+            boardid:'123',
+            topicid:'123',
+            time:"2018-05-01 12:00:00",
+            replyNUM:"5"
+        }],
+
+
+        LatestList: [{
+            key: '1',
+            board: '数据库-高云君',
+            title: "这是一个帖子的标题",
+            author: "我是发帖人ID",
+            boardid:'123',
+            topicid:'123',
+            time:"2018-05-01 12:00:00",
+            replyNUM:"5"
+        }, {
+            key: '2',
+            board: '数据结构基础-陈越',
+            title: "这是一个帖子的标题",
+            author: "我是发帖人ID",
+            boardid:'123',
+            topicid:'123',
+            time:"2018-05-01 12:00:00",
+            replyNUM:"5"
+        }, {
+            key: '3',
+            board: '计算机组成-楼学庆',
+            title: "这是一个帖子的标题",
+            author: "我是发帖人ID",
+            boardid:'123',
+            topicid:'123',
+            time:"2018-05-01 12:00:00",
+            replyNUM:"5"
+        }],
+
+
+    },
+
+    reducers: {
+        updateAllBoardInfo(state, payload) {
+            return {...state,...payload.payload};
+        }
+    },
+    effects: {
+        * getallboard(payload: {payload:{newuser:string}}, {call, put}) {
+
+            yield put({type: 'updateAllBoardInfo', payload: {uid :'test'}});
+
+            return ;
+        },
+
+        * gotoAllBoard(payload: {payload:{}}, {call, put}) {
+
+            yield put(routerRedux.push({
+                pathname: "/allboard",
+            }));
+
+            return ;
+        },
+
+        *show({ payload },{select,call, put}){
+            const name = yield select(state =>state);
+            console.log("in show:",name.allboard.uid);
+
+            return ;
+        },
+
+        *setHotType(payload: {payload: {type:string}}, {call, put}) {
+
+            const selected = payload.payload.type;
+            if(selected=="1"){
+
+            }else if(selected=="7"){
+
+            }else if(selected=="30"){
+
+            }
+
+        },
+    }
+};
+
+export default model;
