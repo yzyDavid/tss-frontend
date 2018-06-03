c4c48b8e2f4e6cf4d1348ce7fb01dd2f1dc88b1f
wenerwu <1271229853@qq.com>
2018-06-03 14:01:59 +0800

    update1

--- src/index.tsx
+++ src/index.tsx
@@ -75,16 +75,7 @@ import StudentSelectionComponent from './components/SelectionStudent';
 import ClassSelectionComponent from './components/SelectionClass';
 import StudentListComponent from './components/StudentList';
 
-import ForumHomePageComponent from './components/ForumHomePage';
-import ForumUserPageComponent from './components/ForumUserPage'
-import MyPostPageComponent from './components/ForumMyPostPage'
-import ReplyPageComponent from './components/ForumReplyPage'
-import NewTopicPageComponent from './components/ForumNewTopicPage'
-import LetterPageComponent from './components/ForumMailPage'
-import BoardPageComponent from './components/ForumBoardPage'
-import TopicPageComponent from './components/ForumTopicPage'
-import SearchComponent from "./components/ForumSearchPage"
-import AllBoardComponent from "./components/ForumAllBoard"
+
 import scoreUploadComponent from './components/ScoreUpload';
 import applyModifyComponent from './components/ApplyModify';
 import scoreManagerComponent from './components/ScoreManager'
@@ -287,59 +278,8 @@ const StuListPage = connect(state =>{
 
 })(StudentListComponent);
 
-const ForumTopicPage = connect(state => {
-    const data = state.topic.allstate;
-    return {allstate: data};
-})(TopicPageComponent);
-
-const ForumReplyPage = connect(state => {
-    const data = state.replyList.replylist;
-    return {ReplyList: data};
-})(ReplyPageComponent);
-
-const ForumSearchPage = connect(state => {
-    const data = state.search.data;
-    return {boardList: data}
-})(SearchComponent);
-
-const ForumAllBoardPage = connect(state => {
-    const data = state.ForumAllBoard.list;
-    return {boardList:data};
-})(AllBoardComponent);
-
-const ForumHomePage = connect(state => {
-    const dataSource = state.ForumAllBoard.uid;
-    const mylist = state.myboard.list;
-    const hot = state.forumhome.HotList;
-    const latest = state.forumhome.LatestList;
-    return {uid: dataSource,alllist :state.ForumAllBoard.list,mylist:mylist,hot:hot,latest:latest };
-})(ForumHomePageComponent);
-
-const ForumUserPage = connect(state => {
-    const data = state.ForumUserInfo.userInfo;
-    return {userInfo: data};
-})(ForumUserPageComponent);
-
-const ForumMyPostPage = connect(state => {
-    const data = state.mypost.postList;
-    return {postList: data};
-})(MyPostPageComponent);
 
 
-const ForumLetterPage = connect(state => {
-    const data = state.mail.input;
-    return {allstate: data}
-})(LetterPageComponent);
-
-const ForumNewTopicPage = connect(state => {
-    const data = {boardName: state.board.boardState.BoardName, boardID: state.board.boardState.BoardID};
-    return {topicBoardInfo: data}
-})(NewTopicPageComponent);
-
-const ForumBoardPage = connect(state => {
-    const data = state.board.boardState;
-    return {boardinfo: data}
-})(BoardPageComponent);
 
 const ScoreUploadPage = connect(state => {
     const {uid, level} = state.login;
@@ -407,18 +347,6 @@ app.router(({history}) => (
 
 
 
-                        <Route exact path="/home" component={ForumHomePage}/>
-                        <Route exact path="/userinfo" component={ForumUserPage}/>
-                        <Route path="/uid=:uid" component={ForumUserPage}/>
-                        <Route exact path="/userarticle" component={ForumUserPage}/>
-                        <Route exact path="/mypost" component={ForumMyPostPage}/>
-                        <Route exact path="/privateLetter" component={ForumLetterPage}/>
-                        <Route exact path="/reply" component={ForumReplyPage}/>
-                        <Route exact path="/search" component={ForumSearchPage}/>
-                        <Route exact path="/newpost" component={ForumNewTopicPage}/>
-                        <Route exact path="/allboard" component={ForumAllBoardPage}/>
-                        <Route path="/board=:boardid" component={ForumBoardPage}/>
-                        <Route path="/topic=:topicid" component={ForumTopicPage}/>
                         <Route path="/scoreUpload" component={ScoreUploadPage} />
                         <Route path="/applyModify" component={ApplyModifyPage} />
                         <Route path="/scoreManager" component={ScoreManagerPage} />
